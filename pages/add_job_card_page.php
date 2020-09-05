347b4e15570ebebc965ca20caa291137769e8709
naflingalaxy <naflingalaxy@gmail.com>
2020-09-05 15:11:01 +0530

    job add fields added

--- pages/add_job_card_page.php
+++ pages/add_job_card_page.php
@@ -11,7 +11,7 @@
                            </div>
                         </div>
                         <div class="iq-card-body">
-                           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vulputate, ex ac venenatis mollis, diam nibh finibus leo</p>
+                           
                            <form>
                               <div class="form-row">
                                  <div class="col-md-6 mb-3">
@@ -43,7 +43,7 @@
                                            if ($item_date) {
                                               for ($x=0; $x < count($item_date); $x++) { 
                                        ?>
-                                       <option><?php echo $item_date[$x]['item_name']; ?></option>
+                                       <option value="<?php echo $item_date[$x]['item_id']; ?>"><?php echo $item_date[$x]['item_name']; ?></option>
                                     <?php }} ?>
                                     </select>
                                     <div class="invalid-tooltip">
@@ -60,13 +60,94 @@
                               </div>
                               <div class="form-group">
                                  <div class="form-check">
+                                 </div>
+                              </div>
+                              <div class="d-flex justify-content-between">
+                                 <div class="iq-header-title">
+                                    <h4 class="card-title">Preferences</h4>
+                                 </div>
+                              </div>
+                              <div class="form-row">
+                                 <div class="col-md-6 mb-3">
+                                    <label for="validationTooltip04">Bank paper</label>
+                                    <select class="custom-select" id="validationTooltip04" required>
+                                       <option selected disabled value="">Bank paper</option>
+                                       
+                                       <option value="">Long - 60</option>
+                                       <option value="">Long - 70</option>
+                                       <option value="">Long - 80</option>
+                                       <option value="">Long - 100</option>
+                                       <option value="">short - 60</option>
+                                       <option value="">short - 70</option>
+                                       <option value="">short - 80</option>
+                                       <option value="">short - 100</option>
+
                                     
+                                    </select>
+                                 </div>
+                                 <div class="col-md-6 mb-3">
+                                    <label for="validationTooltip04">Art Paper</label>
+                                    <select class="custom-select" id="validationTooltip04" required>
+                                       <option selected disabled value="">Art Paper</option>
+                                       
+                                       <option value="">Art Paper - 100</option>
+                                       <option value="">Art Paper - 120</option>
+                                       <option value="">Art Paper - 150</option>
+                                       
+
+                                    
+                                    </select>
+                                 </div>
+                                 <div class="col-md-6 mb-3">
+                                    <div class="d-flex justify-content-between">
+                                       <div class="iq-header-title">
+                                          <h4 class="card-title">NCR</h4>
+                                       </div>
+                                    </div>
+                                    <label for="validationTooltip04">Art Paper</label>
+                                    <select class="custom-select" id="validationTooltip04" required>
+                                       <option selected disabled value="">Bank paper</option>
+                                       
+                                       <option value="">Art Paper - 100</option>
+                                       <option value="">Art Paper - 120</option>
+                                       <option value="">Art Paper - 150</option>
+                                       
+
+                                    
+                                    </select>
+                                 </div>
+                                 <div class="col-md-6 mb-3">
+                                    <label for="validationDefaultUsername">E-mail</label>
+                                    <div class="input-group">
+                                       <div class="input-group-prepend">
+                                          <span class="input-group-text" id="inputGroupPrepend2">@</span>
+                                       </div>
+                                       <input type="text" class="form-control" id="validationDefaultUsername"  aria-describedby="inputGroupPrepend2" required>
+                                    </div>
+                                 </div>
+                                 <div class="col-md-6 mb-3">
+                                    <label for="validationTooltip04">Item</label>
+                                    <select class="custom-select" id="validationTooltip04" required>
+                                       <option selected disabled value="">Choose...</option>
+                                       
+                                       <option value="">hi</option>
+                                       <option value="">hi</option>
+                                    
+                                    </select>
+                                 </div>
+                                 <div class="col-md-6 mb-3">
+                                    <label for="validationDefault03">QTY</label>
+                                    <input type="text" class="form-control" id="validationDefault03" required>
+                                 </div>
+                                 <div class="col-md-6 mb-3">
                                     
                                  </div>
                               </div>
+
                               <div class="form-group">
                                  <button class="btn btn-primary" type="submit">Submit form</button>
                               </div>
+
                            </form>
                         </div>
                      </div>
